081175727c5108ed3d7b6aa3b71cb434cc94c350
Amona Souliman <ammona.souliman1@gmail.com>
2014-10-10 19:28:45 +0200

    Add: Rule and tests for the FOR nested loops

---
+++ TP3/src/main/java/vv/tp3/ForLoopsMustNotBeNestedRule.java
@@ -0,0 +1,23 @@
+package vv.tp3;
+
+import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTForStatement;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+
+/**
+ * Created by amona on 08/10/14.
+ */
+public class ForLoopsMustNotBeNestedRule extends AbstractJavaRule {
+
+    public Object visit(ASTForStatement node, Object data) {
+        if (hasForLoopChild(node)) {
+            addViolation(data, node);
+        }
+
+        return super.visit(node,data);
+    }
+
+    private boolean hasForLoopChild(Node node) {
+        return node.hasDescendantOfType(ASTForStatement.class);
+    }
+}
---
+++ TP3/src/main/java/vv/tp3/NestedForLoopsTest.java
@@ -0,0 +1,19 @@
+package vv.tp3;
+
+/**
+ * Created by Thomas & Amona on 08/10/14.
+ */
+public class NestedForLoopsTest {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.println("Noob");
+            }
+        }
+
+    }
+
+}
