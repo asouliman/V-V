44d11919be526b55f6f9142dee8e65a1c073a028
Amona Souliman <ammona.souliman1@gmail.com>
2014-10-13 14:44:45 +0200

    Add: Tests for move, attack, suffer & isDead methods for the Pawn class

--- TP2/src/test/java/tests/TestPawn.java
+++ TP2/src/test/java/tests/TestPawn.java
@@ -31,54 +31,79 @@ public class TestPawn {
 
 
     /**
-     * Test to get the board width
+     * Test the move method of the Pawn class.
      *
      * @see Pawn#move(Direction)
      * @type Functional
      * @input Pawn('p', 1, 1)
-     * @oracle The X and Y values must change accordingly
+     * @oracle The X and Y values must change accordingly.
      * @passed No
+     * @correction
+     * <pre>
+     *     l.105-106
+     *     - newy > 0
+     *     - newx > 0
+     *     + newy >= 0
+     *     + newx >= 0
+     * </pre>
      */
     @Test
-    public void testMoveNoException() {
+    public void testMoveNoException() throws OutOfBoardException {
         Pawn p = create('p', 1, 1);
 
-        try {
-            p.move(Direction.Up);
-            assertEquals(p.getY(), 2);
-            p.move(Direction.Down);
-            assertEquals(p.getY(), 1);
-            p.move(Direction.Left);
-            assertEquals(p.getX(), 0);
-            p.move(Direction.Right);
-            assertEquals(p.getX(), 1);
-        } catch (OutOfBoardException e) {
-
-        }
+        p.move(Direction.Up);
+        assertEquals(p.getY(), 2);
+        p.move(Direction.Down);
+        assertEquals(p.getY(), 1);
+        p.move(Direction.Left);
+        assertEquals(p.getX(), 0);
+        p.move(Direction.Right);
+        assertEquals(p.getX(), 1);
     }
 
     /**
-     * TODO
+     * Test the move method of the Pawn class.
+     *
+     * @see Pawn#move(Direction)
+     * @type Functional
+     * @input Pawn('p', 1, 1)
+     * @oracle It must throw an OutOfBoardException.
+     * @passed Yes
      */
-    @Test
-    public void testMoveException() {
+    @Test(expected = OutOfBoardException.class)
+    public void testMoveException() throws OutOfBoardException {
         Pawn p = create('p', 1, 1);
 
-        try {
-            p.move(Direction.Left);
-            p.move(Direction.Left);
-            p.move(Direction.Left);
-            p.move(Direction.Left);
-        } catch (OutOfBoardException e) {
-            //System.out.println("OutOfBoardException");
-        }
+        p.move(Direction.Left);
+        p.move(Direction.Left);
     }
 
+    /**
+     * Test the move, attack, suffer & isDead methods of the Pawn class.
+     *
+     * @see Pawn#move(Direction)
+     * @type Functional
+     * @input Pawn('A', 1, 1), Pawn('B', 2, 1)
+     * @oracle The pawn A must kill the pawn B.
+     * @passed Yes
+     */
     @Test
-    public void testIsDead() {
-        Pawn p = create('p', 0, 0);
-        Pawn p2 = create('e', 1, 1);
+    public void testMoveAttack() throws OutOfBoardException {
 
-        assertEquals(false, p.isDead());
+        Board mockBoard = mock(Board.class);
+        when(mockBoard.getXSize()).thenReturn(3);
+        when(mockBoard.getYSize()).thenReturn(3);
+        when(mockBoard.isBonusSquare(1,1)).thenReturn(true);
+
+        Pawn p1 = new Pawn('A', 1, 1, mockBoard);
+        Pawn p2 = new Pawn('B', 2, 1, mockBoard);
+
+        when(mockBoard.getSquareContent(2,1)).thenReturn(p2);
+
+        String msg = p1.move(Direction.Right);
+
+        assertEquals(msg, "A attacks!\nB loses 2 hitpoints.B is dead.");
+        assertTrue(p2.isDead());
     }
+
 }
