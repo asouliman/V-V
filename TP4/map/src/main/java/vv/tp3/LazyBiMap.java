50b3b24e042852881160256293392728f252c15b
Thomas Daniellou <thomas.danll@gmail.com>
2014-10-15 10:24:56 +0200

    Add: TP4

---
+++ TP4/map/src/main/java/vv/tp3/LazyBiMap.java
@@ -0,0 +1,11 @@
+package vv.tp3;
+
+
+import vv.tp3.factory.Factory;
+
+
+public class LazyBiMap<K,V> extends BiMap<K,V> {
+
+    public LazyBiMap(Factory<K, V> keyFactory, Factory<V, K> valueFactory) {
+   }
+}
